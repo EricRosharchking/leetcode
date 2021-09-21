9d38ff96319acabefa52225d120c5fb5b3184c99
Liyuan <liluoke9@gmail.com>
2021-09-21 15:37:51 +0800

    problem 1 solution, brute force and hashmap methods

--- problem1.java
+++ problem1.java
@@ -1,3 +1,30 @@
+import java.util.HashMap;
+
 public class problem1 {
+    public int[] twoSum(int[] nums, int target) {
+        // brute force: O(N*N)
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             return new int[] {i,j};
+        //         }
+        //     }
+        // }
+
+        // better than brute force : use hashmap, choose key, value pair carefully
+        HashMap<Integer, Integer> map = new HashMap<>(); 
+        for (int i = 0; i < nums.length; i ++) {
+            map.put(target - nums[i], i);
+        }
+
+        for(int i = 0; i < nums.length; i ++) {
+            if (map.keySet().contains(nums[i]) && i != map.get(nums[i])) {
+                return new int[] {i, map.get(nums[i])};
+            }
+        }
+        
+
+        return null;
+    }
 
 }
