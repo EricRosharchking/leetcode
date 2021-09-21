9881c1ce392879e45328775018b79d820a865c08
Liyuan <liluoke9@gmail.com>
2021-09-22 00:53:18 +0800

    problem2 with recursive solution on listnode value sum

--- problem2.java
+++ problem2.java
@@ -22,11 +22,14 @@ public class problem2 {
         ListNode l1 = new ListNode(5, l2);
 
 
-        System.out.println(convertListNodeToNum(l4));
-        System.out.println(convertListNodeToNum(l1));
+        // System.out.println(convertListNodeToNum(l4));
+        // System.out.println(convertListNodeToNum(l1));
 
-        System.out.println(convertNumToListNode(BigInteger.valueOf(807)));
+        // System.out.println(convertNumToListNode(BigInteger.valueOf(807)));
+        System.out.println(new problem2().addTwoNumbers(l1, l4));
     }
+
+    /*
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         // brute force: convert the lostnodes to two numbers, add them and conver the
         // result to listnode
@@ -55,5 +58,80 @@ public class problem2 {
             node.next = convertNumToListNode(i.divide(BigInteger.TEN));
         }
         return node;
+    }
+    */
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        /*
+        starting point:
+        two node
+        */
+        //base case
+
+        /*
+        middle point:
+        two node, and 1
+        two node, and 0
+        one node, and 1
+        one node, and 0
+        */
+        if (l1.next == null && l2.next != null) {
+            return new ListNode ((l1.val + l2.val)%10, addTwoNumbers((l1.val + l2.val)/10, l2.next));
+        } else if (l1.next != null && l2.next == null) {
+            return new ListNode ((l1.val + l2.val)%10, addTwoNumbers((l1.val + l2.val)/10, l1.next));
+        } else if (l1.next != null && l2.next != null) {
+            return new ListNode ((l1.val + l2.val)%10, addTwoNumbers((l1.val + l2.val)/10, l1.next, l2.next));
+        } else if (l1.next == null && l2.next == null) {
+            if ((l1.val + l2.val) >= 10) {
+                return new ListNode((l1.val + l2.val)%10, new ListNode (1));
+            } else {
+                return new ListNode(l1.val + l2.val);
+            }
+        }
+
+        return null;
+    }
+    
+    public ListNode addTwoNumbers(int n, ListNode l1, ListNode l2) {
+        if (n == 0) {
+            return addTwoNumbers(l1, l2);
+        } else {
+            if (l1.next == null && l2.next != null) {
+                return new ListNode ((n + l1.val + l2.val)%10, addTwoNumbers((n + l1.val + l2.val)/10, l2.next));
+            } else if (l1.next != null && l2.next == null) {
+                return new ListNode ((n + l1.val + l2.val)%10, addTwoNumbers((n + l1.val + l2.val)/10, l1.next));
+            } else if (l1.next != null && l2.next != null) {
+                return new ListNode ((n + l1.val + l2.val)%10, addTwoNumbers((n + l1.val + l2.val)/10, l1.next, l2.next));
+            } else if (l1.next == null && l2.next == null) {
+                if ((n + l1.val + l2.val) >= 10) {
+                    return new ListNode((n + l1.val + l2.val)%10, new ListNode (1));
+                } else {
+                    return new ListNode(n + l1.val + l2.val);
+                }
+            }
+        }
+
+        return null;
+    }
+        
+    /*
+    end point
+    one node, and 1
+    one node, and zero
+    two node, and 1
+    two node, and zero
+    */
+    
+    public ListNode addTwoNumbers(int n, ListNode l) {
+        if (l.next == null) {
+            if ((n + l.val) >= 10) {
+                return new ListNode((n + l.val)%10, new ListNode(1));
+            } else {
+                return new ListNode(n + l.val);
+            }
+        } else {
+            return new ListNode((n + l.val)%10, addTwoNumbers((n + l.val)/10, l.next));
+        }
     }
 }
